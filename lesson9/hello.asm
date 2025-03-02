613de22d8292de9016bf4eb161abb8a822ec30e3
Naman1Agarwal <m270036@usna.edu>
2025-03-02 12:28:00 -0500

    update

---
+++ lesson9/hello.asm
@@ -0,0 +1,28 @@
+BITS 64
+
+section .text
+
+section _start:
+
+_start:
+
+
+	; rax - 1, rdi - 1, rsi - &buffer, rdx - 12
+
+	mov al, 1
+	mov rdi, 1
+	mov rdx, 12
+
+	
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ lesson9/sysExit.asm
@@ -0,0 +1,23 @@
+global _start
+section .text
+_start:
+    xor rax,rax
+    xor rdi,rdi
+    xor rsi,rsi
+    xor rdx,rdx
+    xor r14,r14
+    xor r15,r15
+    inc al
+    inc dil
+    mov r14,0x00000a21646c726f
+    mov r15,0x57202c6f6c6c6548
+    push r14
+    push r15
+    mov rsi,rsp
+    mov dl,0xf
+    syscall
+    xor rax,rax
+    xor rdi,rdi
+    mov rdi, 0xa
+    mov al,0x3c
+    syscall
